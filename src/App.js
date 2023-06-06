57f0b2018d65dd6dd2769f161c1072b99acaafb5
Capricia-Thompson <thompsoncapricia@gmail.com>
2023-06-06 13:09:16 -0400

    putting it together assignment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import PersonCard from './components/personCard';
 
 const users = [{ "firstName": "Jane", "lastName": "Doe", "age": 45, "hairColor": "Black" }, { "firstName": "John", "lastName": "Smith", "age": 88, "hairColor": "Brown" }, { "firstName": "Millard", "lastName": "Fillmore", "age": 50, "hairColor": "Brown" }, { "firstName": "Maria", "lastName": "Smith", "age": 62, "hairColor": "Brown" }]
 
-
 function App() {
   return (
     <div className="App">
--- src/components/personCard.js
+++ src/components/personCard.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 const PersonCard = (props) => {
     const { firstName, lastName, age, hairColor } = props;
+    const [state, setState] = useState({ 'age': age })
+    const addYear = () => {
+        setState({
+            age: state.age + 1
+        })
+    }
     return (
         <div>
             <h1>{lastName},{firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {state.age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={addYear}>Happy Birthday {firstName}!</button>
         </div>
     )
 }
